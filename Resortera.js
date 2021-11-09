4e66fe96197dabd2a99fa384be96d58fae15e9e2
CariLess <90604545+CariLess@users.noreply.github.com>
2021-11-09 17:35:20 -0600

    Add files via upload

---
+++ Resortera.js
@@ -0,0 +1,22 @@
+class Resortera {
+    constructor(bodyA, pointB) {
+        //Formato JSON
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.resorte = Constraint.create(options);
+        World.add(mundo, this.resorte);
+    }
+display(){
+    var pointA = this.resorte.bodyA.position;
+    var pointB = this.pointB;
+    strokeWeight(3);
+  //line(x1,y1,x2,y2);
+  line(pointA.x,pointA.y,pointB.x,pointB.y);
+}
+
+}
